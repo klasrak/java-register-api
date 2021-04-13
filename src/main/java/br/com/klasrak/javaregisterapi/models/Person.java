63e1236914435d2090b3daefebe0b3bba6ff84ab
Danilo Augusto <dasf_cm@terra.com.br>
2021-04-12 21:36:51 -0300

    feat: create person model

---
+++ src/main/java/br/com/klasrak/javaregisterapi/models/Person.java
@@ -0,0 +1,62 @@
+package br.com.klasrak.javaregisterapi.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
+
+    private static final long serialVersionUId = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Name is required")
+    private String name;
+
+    private String gender;
+
+    @NotEmpty(message = "E-mail is required")
+    private Email email;
+
+    @NotEmpty(message = "Birt date is required")
+    private LocalDateTime birthDate;
+
+    private String hometown;
+
+    private String nationality;
+
+    @NotEmpty(message = "CPF is required")
+    private String cpf;
+
+}
+
+// • Nome - obrigatório
+// • Sexo
+// • E-mail - não obrigatório, deve ser validado caso preenchido
+// • Data de Nascimento - obrigatório, deve ser validada
+// • Naturalidade
+// • Nacionalidade
+// • CPF - obrigatório, deve ser validado (formato e não pode haver dois
+// cadastros com mesmo cpf)
