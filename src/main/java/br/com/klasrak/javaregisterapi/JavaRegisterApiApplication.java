29fe40735cd257be984446326e4ba99cb33ffcc6
Danilo Augusto <dasf_cm@terra.com.br>
2021-04-12 21:16:41 -0300

    chore: first commit

---
+++ src/main/java/br/com/klasrak/javaregisterapi/JavaRegisterApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.klasrak.javaregisterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRegisterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/klasrak/javaregisterapi/JavaRegisterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.klasrak.javaregisterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRegisterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
