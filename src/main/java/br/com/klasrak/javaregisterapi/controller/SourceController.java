58a74bce0bfa3b13348f8b646007459968334e66
Danilo Augusto <dasf_cm@terra.com.br>
2021-04-13 02:27:33 -0300

    feat: add swagger

--- src/main/java/br/com/klasrak/javaregisterapi/controller/SourceController.java
+++ src/main/java/br/com/klasrak/javaregisterapi/controller/SourceController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api("Source Controller")
 @RequestMapping("/api")
 public class SourceController {
+    @ApiOperation(value = "Get source code")
     @GetMapping(value = "/source")
     public String getSource() {
         return "https://github.com/klasrak/java-register-api";
